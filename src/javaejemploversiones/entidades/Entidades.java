d4fe1be03e25e4d0f88ac6488865e1b076bfa918
facundocs2484 <141705639+facundocs2484@users.noreply.github.com>
2023-08-08 16:09:01 -0300

    Create Entidades.java

---
+++ src/javaejemploversiones/entidades/Entidades.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaejemploversiones.entidades;
+
+/**
+ *
+ * @author GOBIERNO
+ */
+public class Entidades {
+    
+}
